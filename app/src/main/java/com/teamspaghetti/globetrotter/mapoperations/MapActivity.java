f3688b40981c79e998ad44f46422cc089977a1fc
msalihguler <muhammedsalihguler@gmail.com>
2016-03-21 16:32:28 +0200

    MapScreen created

--- app/src/main/java/com/teamspaghetti/globetrotter/mapoperations/MapActivity.java
+++ app/src/main/java/com/teamspaghetti/globetrotter/mapoperations/MapActivity.java
@@ -2,6 +2,9 @@ package com.teamspaghetti.globetrotter.mapoperations;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,8 +12,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.teamspaghetti.globetrotter.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by msalihguler on 15.03.2016.
@@ -18,25 +24,48 @@ import com.teamspaghetti.globetrotter.R;
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
-    @Override
+    Button drawroute;
+    ArrayList<Marker> markerList;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.maps);
+        markerList = new ArrayList<Marker>();
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-
-
+        drawroute=(Button)findViewById(R.id.drawroute);
+        drawroute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMap.clear();
+            }
+        });
         mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+        @Override
+        public void onMapLongClick(LatLng latLng) {
 
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+            Marker m_id = mMap.addMarker(new MarkerOptions().position(latLng));
+            markerList.add(m_id);
+            Log.e("markers", markerList.toString());
+        }
+        });
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                markerList.remove(marker);
+                marker.remove();
+                Log.e("markers", markerList.toString());
+                return false;
+            }
+        });
+                // Add a marker in Sydney, Australia, and move the camera.
+                LatLng sydney = new LatLng(-34, 151);
+
         CameraPosition cameraPosition = new CameraPosition.Builder()
                 .target(sydney)      // Sets the center of the map to Mountain View
                 .zoom(10)                   // Sets the zoom

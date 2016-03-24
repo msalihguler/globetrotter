75dfc7cfe9383d1246ca02046839528f7bfef8aa
msalihguler <muhammedsalihguler@gmail.com>
2016-03-24 12:11:37 +0200

    New Files Added

---
+++ app/src/main/java/com/teamspaghetti/globetrotter/CreateTravelPlan.java
@@ -0,0 +1,16 @@
+package com.teamspaghetti.globetrotter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by msalihguler on 24.03.2016.
+ */
+public class CreateTravelPlan extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.createtravelplan);
+    }
+}

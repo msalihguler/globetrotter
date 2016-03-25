23d481aad9c422e6ef9adec30ebb7f757149ab63
msalihguler <muhammedsalihguler@gmail.com>
2016-03-25 17:21:51 +0200

    HTTP Request set up for route

--- app/src/main/java/com/teamspaghetti/globetrotter/CreateTravelPlan.java
+++ app/src/main/java/com/teamspaghetti/globetrotter/CreateTravelPlan.java
@@ -2,11 +2,22 @@ package com.teamspaghetti.globetrotter;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
 /**
  * Created by msalihguler on 24.03.2016.
@@ -17,16 +28,19 @@ public class CreateTravelPlan extends Activity {
     Button saveRoute;
     String url = "192.168.1.251:3000/saveroute";
     ProgressDialog pDialog;
-    String title_send,explanation_send,city,latitude,longitude,location_details;
+    String title_send,explanation_send,city,latitude,longitude,location_details,creator;
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.createtravelplan);
+        sharedPreferences=getSharedPreferences("appPrefs",0);
         title = (EditText)findViewById(R.id.input_tourtitle);
         explanation = (EditText)findViewById(R.id.input_tourdetail);
         saveRoute=(Button)findViewById(R.id.saveroute);
         title_send=title.getText().toString();
+        city="Ankara";
         explanation_send=explanation.getText().toString();
         latitude=getIntent().getExtras().getString("latitude");
         longitude=getIntent().getExtras().getString("longitude");
@@ -35,11 +49,11 @@ public class CreateTravelPlan extends Activity {
         saveRoute.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new SaveRoute().execute();
+                new SaveRoute().execute(url);
             }
         });
     }
-    class SaveRoute extends AsyncTask<String,String,String>{
+    class SaveRoute extends AsyncTask<String, String, Integer> {
         @Override
         protected void onPreExecute() {
             pDialog= new ProgressDialog(CreateTravelPlan.this);
@@ -49,13 +63,52 @@ public class CreateTravelPlan extends Activity {
         }
 
         @Override
-        protected String doInBackground(String... params) {
+        protected Integer doInBackground(String... params) {
+            Integer result = 0;
+            HttpURLConnection urlConnection;
+            try {
+                URL url = new URL(params[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.setRequestMethod("POST");
+                String charset = "UTF-8";
 
+                String s = "{\"title\":\"" + URLEncoder.encode(title_send, charset)+"\",";
+                s += "{\"latitude\":\"" + URLEncoder.encode(latitude, charset)+"\",";
+                s += "{\"longitude\":\"" + URLEncoder.encode(longitude, charset)+"\",";
+                s += "{\"location_details\":\"" + URLEncoder.encode(title_send, charset)+"\",";
+                s += "{\"city\":\"" + URLEncoder.encode(city, charset)+"\",";
+                s += "{\"creator\":\"" + URLEncoder.encode(sharedPreferences.getString("username","default_name"), charset)+"\",";
+                s += "\"detail\":\"" + URLEncoder.encode(explanation_send, charset)+"\"}";
 
-            return null;
+                urlConnection.setFixedLengthStreamingMode(s.getBytes().length);
+                PrintWriter out = new PrintWriter(urlConnection.getOutputStream());
+                out.print(s);
+                out.close();
+                int statusCode = urlConnection.getResponseCode();
+
+                Log.e("status", String.valueOf(statusCode));
+
+                if (statusCode == 255) {
+                    BufferedReader r = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = r.readLine()) != null) {
+                        response.append(line);
+                    }
+                    JSONObject jsonObject = new JSONObject(response.toString());
+
+                    result = 1; // Successful
+                } else {
+                    result = statusCode; //"Failed to fetch data!";
+                }
+            } catch (Exception e) {
+                Log.d("sadwd", e.getLocalizedMessage());
+            }
+            return result; //"Failed to fetch data!";
         }
         @Override
-        protected void onPostExecute(String s) {
+        protected void onPostExecute(Integer s) {
             super.onPostExecute(s);
             pDialog.dismiss();
         }
